a4ea6b5ab2b009752ff2d6c973ef3f49d5c48b28
jothimani15 <jothimani5255@gmail.com>
2025-11-07 14:49:49 +0530

    commit

---
+++ Dependency-Injection-with-Autowiring/src/main/java/com/example/demo/DependencyInjectionWithAutowiringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionWithAutowiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionWithAutowiringApplication.class, args);
+	}
+
+}
